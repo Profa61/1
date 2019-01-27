ddbf7eac1438aaac98c1042a2b9eb4b90330793d
Profa61 <46356689+Profa61@users.noreply.github.com>
2019-01-27 20:18:19 +0600

    добавлены флажки и пищалка

---
+++ vklychenie_sveta/vklychenie_sveta/autos.ino
@@ -0,0 +1,63 @@
+void autos(void) {
+  //// автомат включения света в туалете
+  switch (svet_tualet) {
+    case 0:
+      svetTualetMs = ms;
+      break;
+
+    case 10:
+      if ((ms - svetTualetMs) > 1000) {
+        svetTualetMs = ms;
+        digitalWrite(  LED_TUALET, 1);
+        Serial.println("СВЕТ В ТУАЛЕТЕ ВКЛЮЧЕН");
+        TUALET_FLAG = 1;
+        svet_tualet = 0; //GO
+      } //ms
+      
+      break;
+
+    case 11:
+      if ((ms - svetTualetMs) > 1000) {
+        svetTualetMs = ms;
+        digitalWrite(  LED_TUALET, 0);
+        Serial.println("СВЕТ В ТУАЛЕТЕ ВЫКЛЮЧЕН");
+        TUALET_FLAG = 0;
+        svet_tualet = 0; //GO
+
+      } //ms
+      break;
+
+      
+  } //switch(svet_tualet)
+
+  // автомат включения света в ванной
+  switch (svet_vanna) {
+    case 0:
+      svetVannaMs = ms;
+      break;
+
+    case 10:
+      if ((ms - svetVannaMs) > 1000) {
+        svetVannaMs = ms;
+        digitalWrite(LED_VANNA, 1);
+        Serial.println("СВЕТ В ВАННОЙ ВКЛЮЧЕН");
+        VANNA_FLAG = 1;
+        svet_vanna = 0; //GO
+      } //ms
+      break;
+
+    case 11:
+      if ((ms - svetVannaMs) > 1000) {
+        svetVannaMs = ms;
+        digitalWrite(LED_VANNA, 0);
+        Serial.println("СВЕТ В ВАННОЙ ВЫКЛЮЧЕН");
+        VANNA_FLAG = 0;
+        svet_vanna = 0; //GO
+      } //ms
+      break;
+
+      
+     
+  }//switch(svet_vanna)
+}//autos() 
+
---
+++ vklychenie_sveta/vklychenie_sveta/sensors.ino
@@ -0,0 +1,60 @@
+void sensors(void) {
+
+
+  if ( digitalRead (IK_TUALET) == RUKA  && (TUALET_FLAG) == 0) {
+
+    while (!digitalRead(IK_TUALET));
+
+    digitalWrite (BUZZER, 1);
+    delay(20);
+    digitalWrite (BUZZER, 0);
+    svet_tualet = 10;
+
+
+  } //if TUALET
+
+
+  if (digitalRead(IK_TUALET) == RUKA  && (TUALET_FLAG) == 1) {
+
+
+    while (!digitalRead(IK_TUALET));
+
+
+    digitalWrite(BUZZER, 1);
+    delay(20);
+    digitalWrite(BUZZER, 0);
+    svet_tualet = 11;
+
+
+  } //if TUALET
+
+
+
+  if (digitalRead(IK_VANNA) == RUKA && (VANNA_FLAG) == 0) {
+
+
+    while (!digitalRead(IK_VANNA));
+    digitalWrite(BUZZER, 1);
+    delay(20);
+    digitalWrite(BUZZER, 0);
+    svet_vanna = 10;
+
+
+  } //if VANNA
+
+
+  if (digitalRead(IK_VANNA) == RUKA && (VANNA_FLAG) == 1) {
+
+
+    while (!digitalRead(IK_VANNA));
+
+    digitalWrite (BUZZER, 1);
+    delay(20);
+    digitalWrite(BUZZER, 0);
+    svet_vanna = 11;
+
+
+  } //if VANNA*/
+
+}//sensors()
+
